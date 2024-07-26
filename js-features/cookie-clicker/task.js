fb617d5ed620681ce8df80869f2409aa2a10241a
Sasha Pudkova <sashapudkova@bk.ru>
2024-07-26 17:27:34 +0300

    second commit for cookies

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,16 +1,16 @@
 const clicker = document.getElementById('clicker__counter')
 const cookieClick = document.getElementById('cookie')
 
+let clickNumber = 0;
+
 cookieClick.onclick = function() {
-    clicker.textContent++;
-    cookieClick.width = 250
 
-//resetImg()
+    clickNumber++;
+    if (clickNumber % 2 === 0) {
+        cookieClick.width +=50;
+    } else {
+        cookieClick.width -=50;
+    }
+
+    clicker.textContent = clickNumber;
 }
- 
-
-//function resetImg() {
-//cookieClick.classList.toggle('clicker__cookie')
-//}
-
-
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -5,26 +5,27 @@ const lost = document.getElementById('lost')
 
 let getHole = index => document.getElementById(`hole${index}`)
 
-    for (i = 0; i < 10; i++) {
+    for (i = 1; i < 10; i++) {
         let holeItem = getHole(i)
 
         holeItem.addEventListener('click', () => {
             if (holeItem.className.includes('hole_has-mole')) {
                 dead.textContent++
-            } 
-              lost.textContent++
+            } else {
+                lost.textContent++
+            };
+
+            if (lost.textContent === '10') {
+                alert('Вы проиграли')
+                dead.textContent = '0';
+                lost.textContent = '0';
+            }
+            
+            if (dead.textContent === '5') {
+                alert('Вы выиграли')
+                dead.textContent = '0';
+                lost.textContent = '0';
+            }
         })
-    
     }
 
-if (lost.textContent === 10) {
-    alert('Вы выиграли')
-    dead.textContent === 0;
-    lost.textContent === 0;
-}
-
-if (dead.textContent === 5) {
-    alert('Вы проиграли')
-    dead.textContent === 0;
-    lost.textContent === 0;
-}
