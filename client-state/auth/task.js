fb0c4283deb38cb39b838065e84e87cab3bdf2b2
Sasha Pudkova <sashapudkova@bk.ru>
2024-08-17 12:49:04 +0300

    second commit for task state

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,34 +1,37 @@
-const form = document.forms.signin__form;
-const btn = document.querySelector('.btn')
-const xhr = new XMLHttpRequest();
+let form = document.getElementById('signin__form')
+let btn = document.querySelector('.btn')
 
-const item = localStorage.getItem('item')
+// при обновении страницы для обнуления localStorage
+localStorage.removeItem('id');
 
+let item = localStorage.getItem('id')
 if(item) {
-    document.querySelector('#signin').classList.remove('.signin_active');
-    document.querySelector('.welcome').classList.add('.welcome_active');
+    document.querySelector('#signin').classList.remove('signin_active');
+    document.querySelector('.welcome').classList.add('welcome_active');
     document.querySelector('#user_id').textContent = item;
 }
 
 btn.addEventListener('click', (e) => {
     e.preventDefault();
 
-    xhr.addEventListener('readystatechange', () => {
-        if(xhr.readyState === xhr.DONE) {
-            let data = JSON.parse(xhr.responseText)
-            if (data['success']) {
-
-                document.querySelector('.welcome').classList.add('.welcome_active');
-                localStorage.setItem('item', data['user_id']) 
-                document.querySelector('#user_id').textContent = data['user_id']
-            } else {
-                alert('Неверный логин/пароль')
-            }
-        }
-    })
-
-
+    let xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+    xhr.responseType = 'json';
     const formData = new FormData(form)
     xhr.send(formData);
+    form.reset();
+
+    xhr.onload = function() {
+        let response = xhr.response;
+        if (xhr.status != 201) {
+            alert (`Ошибка ${xhr.status}: ${xhr.statusText}`);
+        } else if (response['success']) {
+                    document.querySelector('#signin').classList.remove('signin_active');
+                    document.querySelector('.welcome').classList.add('welcome_active');
+                    localStorage.setItem('id',response['user_id']) 
+                    document.querySelector('#user_id').textContent = response['user_id']
+            }  else {
+                alert('Неверный логин/пароль');
+            }
+    }
 })
--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,27 +1,25 @@
 let modal = document.querySelector('.modal')
 let modalClose = document.querySelector('.modal__close')
 
-// функция возвращает строку
-function getCookie() {
-    return document.cookie.split('; ').reduce((acc, item) => {
-      const [name, value] = item.split('=')
-      acc[name] = value
-      return acc
-    })
-  }
+function getCookie(name) {
+  return document.cookie.split('; ').reduce((acc, elm) => {
+    if (elm.startsWith(name + '=')) {
+      return elm.slice(name.length + 1);
+    } else {
+      return acc;
+    }
+  }, 0);
+}
   
-  const cookie = getCookie('modal')
+  const cookie = getCookie('cookie')
 
       if(!cookie) {
-        modal.classList.add('.modal_active')
-        document.cookie = 'modal=open'
+          modal.classList.add('modal_active')
+          document.cookie = 'cookie=open'
 
         modalClose.addEventListener('click', (e) => {
             e.preventDefault();
-            modal.classList.remove('.modal_active')
-            document.cookie = 'modal=close'
+            document.cookie = 'cookie=close'
+            modal.classList.remove('modal_active')
         })
     }
-
-
-
